352e7c572a992cba1a9a445bef0ab923490c1655
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-12-06 14:18:37 +0100

    add footer + carousel + equipments + equipments-details components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import {NgOptimizedImage} from "@angular/common";
+import { EquipmentsComponent } from './equipments/equipments.component';
+import { EquipmentDetailsComponent } from './equipment-details/equipment-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
+    CarouselComponent,
+    EquipmentsComponent,
+    EquipmentDetailsComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NgOptimizedImage
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
